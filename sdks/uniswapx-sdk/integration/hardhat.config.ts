d60bf88a31b33a6b8efda4252fb72e6d045efd6f
Travis Reeder <treeder@gmail.com>
2025-02-27 07:52:30 +0000

    Upgrade to ethers 6

--- sdks/uniswapx-sdk/integration/hardhat.config.ts
+++ sdks/uniswapx-sdk/integration/hardhat.config.ts
@@ -1,7 +1,6 @@
-import { HardhatUserConfig } from "hardhat/config";
-import "@nomiclabs/hardhat-ethers";
-import "@nomicfoundation/hardhat-chai-matchers";
-import "@typechain/hardhat";
+import { HardhatUserConfig } from "hardhat/config"
+import "@nomicfoundation/hardhat-chai-matchers"
+import "@typechain/hardhat"
 
 import 'dotenv/config'
 
@@ -16,6 +15,6 @@ const config: HardhatUserConfig = {
       }
     },
   },
-};
+}
 
-export default config;
+export default config
